3aa39b4f1d59265b7161f83bd036c79517101611
André Wohlfahrt <andre128bits@gmail.com>
2018-06-19 08:03:32 -0300

    19062018

---
+++ folha.sql
@@ -0,0 +1,9 @@
+
+select * from r047pen
+
+
+select * from r047his where obstit like '%IRRF Folha%' and numtit='RH0000010H2'
+select * from r047his where numtit='RH0000010H2'
+
+select * from r047his where obstit like '%IRRF Folha%' order by vnctit desc
+select * from r047his where numtit='RH0000010LF'
